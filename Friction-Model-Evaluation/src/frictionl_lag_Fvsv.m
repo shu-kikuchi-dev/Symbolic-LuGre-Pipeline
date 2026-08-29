3c2e297d98919edfc0e68f9195aa65ba7fd2c517
shu-kikuchi-dev <shu.kikuchi.dev@gmail.com>
2026-08-29 18:59:20 +0900

    refine frictional_lag_Fvsv.slx to be LuGre, refine frictional_lag_Fvsv.m to create graph of LuGre, add a new graph that captures frictional lag of LuGre

--- Friction-Model-Evaluation/src/frictionl_lag_Fvsv.m
+++ Friction-Model-Evaluation/src/frictionl_lag_Fvsv.m
@@ -3,11 +3,11 @@ clear; clc;
 
 % --- File Saving Setup ---
 date_str = '26-08-29';    % just a 'date' is built-in function, so better to avoid
-model_code = 'surrogate-model-1';
+model_code = 'LuGre';
 model_settings = 'ode23tb_maxstepsize-1en4_relativetorelance-1en7_absolutetorelance-1en10';
 input_conditions = 'amplitude-1en3_bias-1p5en3_phase-0';
 datafactory_conditions = 'params-paper';
-save_dir = 'C:\Users\shuki\Projects\work\Symbolic-LuGre-Pipeline\Friction-Model-Evaluation\figs\SurrogateModel1';
+save_dir = 'C:\Users\shuki\Projects\work\Symbolic-LuGre-Pipeline\Friction-Model-Evaluation\figs\LuGre';
 file_name = [date_str, '__', model_code, '__', model_settings, '__', input_conditions, '__', datafactory_conditions, '.pdf'];
 
 % Create the folder automatically if it doesn't exist
@@ -61,7 +61,7 @@ end
 % Finalize Graph
 xlabel('Velocity v /(m/s)');
 ylabel('Friction Force F /N');
-title('Frictional Lag: F vs v Hysteresis (Steady State');
+title('Frictional Lag: F vs v Hysteresis (Steady State)');
 legend('show', 'Location', 'best');
 
 % Adjust axes to see the loops clearly
