a83709e0e3d03dfa01bbd0f80f553e5b6bdeeb1b
shu-kikuchi-dev <shu.kikuchi.dev@gmail.com>
2026-08-29 16:21:29 +0900

    refine frictional_lag_Fvsv.slx, wrote the rest of the frictional_lag_Fvsv.m
    changed all ToWorkspace block to take the form of Timeseries, created a new base workspace vafriable named w as the omega for input of velcoity sine wave. wrote the rest of the automation program frictional_lag_Fvsv.m

--- Friction-Model-Evaluation/src/frictionl_lag_Fvsv.m
+++ Friction-Model-Evaluation/src/frictionl_lag_Fvsv.m
@@ -7,8 +7,8 @@ t_stop = 30;
 t_start_plot = 3;
 
 % Velocity Sine Wave Parameters (Bias > Amplitude to ensure unidirectional)
-Bias = 1.5e-3;
-Amp = 1.0e-3;
+%Bias = 1.5e-3;
+%Amp = 1.0e-3;
 
 % Frequencies
 omegas = [1, 10, 25];
@@ -22,3 +22,36 @@ grid on;
 % --- Loop Through Frequencies ---
 for i = 1:length(omegas)
     % Update the workspace variable that Simulink uses
+    w = omegas(i);
+
+    fprintf('Running simulation for omega = %d rad/s...\n', w);
+
+    % Run the simulation
+    simOut = sim(model_name, 'StopTime', num2str(t_stop));
+
+    % Extract Data
+    t = simOut.tout;
+    v = simOut.v_out.Data;
+    F = simOut.F_out.Data;
+
+    % Filter for Steady State
+    idx = find(t > t_start_plot);
+    v_ss = v(idx);
+    F_ss = F(idx);
+
+    % Plot F vs v
+    plot(v_ss, F_ss, colors(i), 'LineWidth', 1.5, ...
+        'DisplayName', sprintf('\\omega = %d rad/s', w));
+end
+
+% Finalize Graph
+xlabel('Velocity v /(m/s)');
+ylabel('Friction Force F /N');
+title('Frictional Lag: F vs v Hysteresis (Steady State');
+legend('show', 'Location', 'best');
+
+% Adjust axes to see the loops clearly
+xlim([0.4e-3, 2.6e-3]);
+
+hold off;
+fprintf('All simulations complete.\n');
