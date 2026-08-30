f233ffc657c172aecfe87aa28c0ecba915e25d13
shu-kikuchi-dev <shu.kikuchi.dev@gmail.com>
2026-08-30 16:04:21 +0900

    refine pre_sliding_Fvsx.slx to take a form of Timeseries than arrey for each ToWorkspace block, rename pre_sliding_Fvsx.m to generate_pre_sliding.m not to occur name conflict error with pre_sliding_Fvsx.slx

--- Friction-Model-Evaluation/src/generate_pre_sliding.m
+++ Friction-Model-Evaluation/src/generate_pre_sliding.m
@@ -22,26 +22,31 @@ t_stop = 10;
 omega = 40;
 
 % --- Figure Initialization ---
-figure;
+fig = figure;
+hold on;
 grid on;
 
 % --- Run the Simulation ---
 fprintf('Running simulation...\n')
-simOut = sim(model_name, 'StopTime', num3str(t_stop));
+simOut = sim(model_name, 'StopTime', num2str(t_stop));
 
 % Extract Data
 t = simOut.tout;
 x = simOut.x_out.Data;
 F = simOut.F_out.Data;
+idx = find(t > 3);
 
 % Plot F vs x
-plot(x(t), F(t));
+plot(x(idx), F(idx));
 xlabel('Displacement x /m');
 ylabel('Friction Force F /N');
 title('Pre-Sliding: F vs x Hysteresis');
 
 % Adjust axes to see the loops clearly
-xlim([-5, 5]);
+xlim([-5e-6, 5e-6]);
+ylim([-0.5, 0.5])
+
+hold off;
 
 % --- Save the fig ---
 full_save_path = fullfile(save_dir, file_name);
