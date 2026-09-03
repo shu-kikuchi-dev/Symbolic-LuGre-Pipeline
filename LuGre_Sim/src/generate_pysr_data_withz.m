7c7fb7fa32f31166a20044772c8cb0d70b79384d
shu-kikuchi-dev <shu.kikuchi.dev@gmail.com>
2026-09-03 13:41:52 +0900

    refine generate_pysr_data_withz.m to change the file saving path

--- LuGre_Sim/src/generate_pysr_data_withz.m
+++ LuGre_Sim/src/generate_pysr_data_withz.m
@@ -9,9 +9,9 @@ clearvars; clc; close all;
 % FOR MY DESKTOP save_fig_dir = 'D:\shu-kikuchi-projects\MATLAB_project\LuGre_Sim\outputs\tmp_figs\MasterData';
 
 % FOR MY LAPTOP
-save_csv_dir = 'C:\Users\shuki\Projects\work\Symbolic-LuGre-Pipeline\LuGre_Sim\outputs\tmp_csv_files';
+save_csv_dir = 'C:\Users\shuki\Projects\work\Symbolic-LuGre-Pipeline\LuGre_Sim\tmp-outputs\tmp_csv_files';
 % FOR MY LAPTOP 
-save_fig_dir = 'C:\Users\shuki\Projects\work\Symbolic-LuGre-Pipeline\LuGre_Sim\outputs\tmp_figs';
+save_fig_dir = 'C:\Users\shuki\Projects\work\Symbolic-LuGre-Pipeline\LuGre_Sim\tmp-outputs\tmp_figs';
 
 csv_name = '26-09-03_script-generatepysrdatawithz_after-big-refine_ode23tbf_maxstepsize-1en4_relativetolerance-1en7_absolutetolerance-1en10';
 fig_name = '26-09-03_script-generatepysrdatawithz_after-big-refine_ode23tbf_maxstepsize-1en4_relativetolerance-1en7_absolutetolerance-1en10';
